8e1a46ddadc312d7de97c658fe6d390eded6618f
HatamJafarov <hatam.jafarov@gmail.com>
2021-03-09 14:43:32 +0400

    update JS file

--- script.js
+++ script.js
@@ -1,54 +1,10 @@
 var keys = document.querySelectorAll('.p');
 var audios = document.querySelectorAll('audio');
 
-keys[0].addEventListener('click', function(e){
-	audios[0].play()
-})
-keys[1].addEventListener('click', function(e){
-	audios[1].play()
-})
-keys[2].addEventListener('click', function(e){
-	audios[2].play()
-})
-keys[3].addEventListener('click', function(e){
-	audios[3].play()
-})
-keys[4].addEventListener('click', function(e){
-	audios[4].play()
-})
-keys[5].addEventListener('click', function(e){
-	audios[5].play()
-})
-keys[6].addEventListener('click', function(e){
-	audios[6].play()
-})
-keys[7].addEventListener('click', function(e){
-	audios[7].play()
-})
-keys[8].addEventListener('click', function(e){
-	audios[8].play()
-})
-keys[9].addEventListener('click', function(e){
-	audios[9].play()
-})
-keys[10].addEventListener('click', function(e){
-	audios[10].play()
-})
-keys[11].addEventListener('click', function(e){
-	audios[11].play()
-})
-keys[12].addEventListener('click', function(e){
-	audios[12].play()
-})
-keys[13].addEventListener('click', function(e){
-	audios[13].play()
-})
-keys[14].addEventListener('click', function(e){
-	audios[14].play()
-})
-keys[15].addEventListener('click', function(e){
-	audios[15].play()
-})
-keys[16].addEventListener('click', function(e){
-	audios[16].play()
-})
+
+for(i = 0; i < keys.length; i++){
+	keys[i].addEventListener('click', function(e){
+		audios[i].play()
+	})
+}
+
